8e8802ae973a92787bb1e1dde9807a4d382762da
Sebastianserr <145410457+Sebastianserr@users.noreply.github.com>
2023-09-27 08:31:15 -0600

    formulario1
    formulario1

---
+++ Formulario11/src/main/java/vjv/formulario11/Formulario11.java
@@ -0,0 +1,13 @@
+package vjv.formulario11;
+import javax.swing.*;
+public class Formulario11 extends JFrame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Formulario11 formulario11=new Formulario11();
+        formulario11.setBounds(10,20,400,300);
+        formulario11.setVisible(true);
+        formulario11.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
